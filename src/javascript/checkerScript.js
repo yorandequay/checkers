8947be4752a3f517e0db92759b7857b86b07305a
Yoran de Quay <yoranq@hotmail.com>
2021-02-26 12:51:09 +0100

    now only highlights tiles where the checker can move to

--- src/javascript/checkerScript.js
+++ src/javascript/checkerScript.js
@@ -5,10 +5,31 @@ for (i = 0; i < blackCheckers.length; i++) {
     const dataBlackPiece = document.getElementById("blackPiece" + i).dataset.blackpiece;
     const elementBlackPiece = document.getElementById("blackPiece" + i);
     document.getElementById("blackPiece" + i).onclick = function moveBlackChecker () {
-        console.log("Black checker: ");
-        console.log(dataBlackPiece);
-        console.log("Tile: ");
         const dataTile = elementBlackPiece.parentElement.dataset.tile;
+        const stepOne = parseInt(dataTile) + 9;
+        const stepTwo = parseInt(dataTile) + 7;
+        const stepThree =  parseInt(dataTile) - 9;
+        const stepFour =  parseInt(dataTile) - 7;
+        if (!document.getElementById("tile" + stepOne).firstChild) {
+            document.getElementById("tile" + stepOne).style.backgroundColor = "yellow";
+        } 
+        if(!document.getElementById("tile" + stepTwo).firstChild) {
+            document.getElementById("tile" + stepTwo).style.backgroundColor = "yellow";
+        } 
+        if (!document.getElementById("tile" + stepThree).firstChild) {
+            document.getElementById("tile" + stepThree).style.backgroundColor = "yellow";
+        } 
+        if (!document.getElementById("tile" + stepFour).firstChild) {
+            document.getElementById("tile" + stepFour).style.backgroundColor = "yellow";
+        }
+    }
+}
+
+for (a = 0; a < whiteCheckers.length; a++) {
+    const dataWhitePiece = document.getElementById("whitePiece" + a).dataset.whitepiece;
+    const elementWhitePiece = document.getElementById("whitePiece" + a);
+    document.getElementById("whitePiece" + a).onclick = function moveWhiteChecker () {
+        const dataTile = elementWhitePiece.parentElement.dataset.tile;
         const stepOne = parseInt(dataTile) + 9;
         const stepTwo = parseInt(dataTile) + 7;
         const stepThree =  parseInt(dataTile) - 9;
@@ -17,17 +38,6 @@ for (i = 0; i < blackCheckers.length; i++) {
         document.getElementById("tile" + stepTwo).style.backgroundColor = "yellow";
         document.getElementById("tile" + stepThree).style.backgroundColor = "yellow";
         document.getElementById("tile" + stepFour).style.backgroundColor = "yellow";
-    }
-}
-
-for (a = 0; a < whiteCheckers.length; a++) {
-    const dataWhitePiece = document.getElementById("whitePiece" + a).dataset.whitepiece;
-    const elementWhitePiece = document.getElementById("whitePiece" + a);
-    document.getElementById("whitePiece" + a).onclick = function moveBlackChecker () {
-        console.log("White checker: ");
-        console.log(dataWhitePiece);
-        console.log("Tile: ");
-        console.log(elementWhitePiece.parentElement.dataset.tile);
     }
 }
 
